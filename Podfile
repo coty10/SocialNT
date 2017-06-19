798e1e213f91f83fff6207e6fd5f1228f0fa6805
Coty10 <mcotugno10@gmail.com>
2017-06-19 12:31:10 +0200

    First commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.3'
+
+target 'SocialNT' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SocialNT
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+
+end
---
+++ SocialNT/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  SocialNT
+//
+//  Created by Marco Cotugno on 19/06/17.
+//  Copyright © 2017 Marco Cotugno. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
