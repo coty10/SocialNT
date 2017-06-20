bdf30b8e49ee9cec78f991f559272275b41ba683
Coty10 <mcotugno10@gmail.com>
2017-06-20 12:51:18 +0200

    Feed user interface completed

---
+++ SocialNT/CircleImageView.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImageView.swift
+//  SocialNT
+//
+//  Created by Marco Cotugno on 20/06/17.
+//  Copyright © 2017 Marco Cotugno. All rights reserved.
+//
+
+import UIKit
+
+class CircleImageView: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+    }
+
+
+}
