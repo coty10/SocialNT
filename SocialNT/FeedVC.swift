425ba1931860e1d9ef107e9b20b3edec2e5dd29c
Coty10 <mcotugno10@gmail.com>
2017-06-21 13:59:04 +0200

    Firebase image retrieving and cache - First

--- SocialNT/FeedVC.swift
+++ SocialNT/FeedVC.swift
@@ -18,6 +18,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     var imagePicker: UIImagePickerController!
     
     var posts = [Post]()
+    static var imgCache: NSCache<NSString, UIImage> = NSCache()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,11 +56,21 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
         
         let post = posts[indexPath.row]
         if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
-            cell.configureCell(post: post)
-            return cell
+            
+            if let img = FeedVC.imgCache.object(forKey: post.imgUrl as NSString) {
+                
+                cell.configureCell(post: post, img: img)
+                
+            } else {
+                
+                cell.configureCell(post: post)
+                return cell
+            }
+            
         } else {
             return PostCell()
         }
+        return PostCell()
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
--- SocialNT/PostCell.swift
+++ SocialNT/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
 
@@ -24,10 +25,29 @@ class PostCell: UITableViewCell {
     }
     
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.captionText.text = post.caption
         self.numberOfLikes.text = "\(post.likes)"
+        
+        if img != nil {
+            self.postImg.image = img
+        } else {
+            let ref = Storage.storage().reference(forURL: post.imgUrl)
+            ref.getData(maxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                
+                if error != nil {
+                    print("Coty10: Unable to get image from Firebase Storage")
+                } else {
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imgCache.setObject(img, forKey: post.imgUrl as NSString)
+                        }
+                    }
+                }
+            })
+        }
         
     }
 
